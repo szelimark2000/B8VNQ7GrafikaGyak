1204d818b54d30a761937aa21887b6957276997f
szelimark2000 <64776810+szelimark2000@users.noreply.github.com>
2021-04-02 12:21:39 +0200

    2021_03_25_cube

---
+++ gyakorlatok/0325/cube/src/scene.c
@@ -0,0 +1,107 @@
+#include "scene.h"
+
+#include <GL/glut.h>
+
+#include <obj/load.h>
+#include <obj/draw.h>
+
+void init_scene(Scene* scene)
+{
+    load_model(&(scene->cube), "cube.obj");
+    load_model(&(scene->hare), "hare.obj");
+    scene->cube_texture_id = load_texture("cube.png"); 
+    scene->hare_texture_id = load_texture("hare.jpg"); 
+
+
+    scene->material.ambient.red = 1.0;
+    scene->material.ambient.green = 1.0;
+    scene->material.ambient.blue = 1.0;
+
+    scene->material.diffuse.red = 1.0;
+    scene->material.diffuse.green = 1.0;
+    scene->material.diffuse.blue = 1.0;
+
+    scene->material.specular.red = 0.0;
+    scene->material.specular.green = 0.0;
+    scene->material.specular.blue = 0.0;
+
+    scene->material.shininess = 0.0;
+}
+
+void set_lighting()
+{
+    float ambient_light[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+    float diffuse_light[] = { 1.0f, 1.0f, 1.0, 1.0f };
+    float specular_light[] = { 0.0f, 0.0f, 0.0f, 0.0f };
+    float position[] = { 0.0f, 0.0f, 0.0f, 0.0f };
+
+    glLightfv(GL_LIGHT0, GL_AMBIENT, ambient_light);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuse_light);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, specular_light);
+    glLightfv(GL_LIGHT0, GL_POSITION, position);
+}
+
+void set_material(const Material* material)
+{
+    float ambient_material_color[] = {
+        material->ambient.red,
+        material->ambient.green,
+        material->ambient.blue
+    };
+
+    float diffuse_material_color[] = {
+        material->diffuse.red,
+        material->diffuse.green,
+        material->diffuse.blue
+    };
+
+    float specular_material_color[] = {
+        material->specular.red,
+        material->specular.green,
+        material->specular.blue
+    };
+
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambient_material_color);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse_material_color);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specular_material_color);
+
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, &(material->shininess));
+}
+
+void draw_scene(const Scene* scene)
+{
+    set_material(&(scene->material));
+    set_lighting();
+    draw_origin();
+
+    glPushMatrix();
+    glBindTexture(GL_TEXTURE_2D, scene->cube_texture_id);
+    draw_model(&scene->cube);
+    glPopMatrix();
+
+    glPushMatrix();
+    glBindTexture(GL_TEXTURE_2D, scene->hare_texture_id);
+    glTranslatef(0, 2, 0);
+    glScalef(0.2, 0.2, 0.2);
+    draw_model(&scene->hare);
+    glPopMatrix();
+    }
+
+void draw_origin()
+{
+    glBegin(GL_LINES);
+
+    glColor3f(1, 0, 0);
+    glVertex3f(0, 0, 0);
+    glVertex3f(1, 0, 0);
+
+    glColor3f(0, 1, 0);
+    glVertex3f(0, 0, 0);
+    glVertex3f(0, 1, 0);
+
+    glColor3f(0, 0, 1);
+    glVertex3f(0, 0, 0);
+    glVertex3f(0, 0, 1);
+
+    glEnd();
+}
